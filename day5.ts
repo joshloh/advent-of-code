04b862be9840f1b3b5f09f41122a062167e71a77
Josh <joshuafloh@gmail.com>
2023-12-05 22:29:21 +1100

    extract input parsing into a function

--- day5.ts
+++ day5.ts
@@ -1,6 +1,23 @@
 import * as fs from 'fs';
 
 // functions
+
+// parse input and return a seeds array and a maps array which contains all maps/transformations
+function parseInput(input: string): {
+  seeds: number[],
+  maps: number[][][]
+} {
+  const split = input.split('\r\n\r\n');
+  const seeds = split[0].split(': ')[1].split(' ').map((e) => parseInt(e));
+  const mapsString = split.slice(1).map((e) => (e.split('\r\n').slice(1)));
+  const maps = mapsString.map((e) => e.map((i) => i.split(' ').map((j) => parseInt(j))));
+  return {
+    seeds,
+    maps
+  }
+}
+
+// traverse a "seed" through a map of transformations and return the final transformed vaLue
 function getLocation(seed: number, maps: number[][][]): number {
   let location = seed;
   for (const map of maps) {
@@ -18,6 +35,7 @@ function getLocation(seed: number, maps: number[][][]): number {
   return location;
 }
 
+// calculate all locations for each seed and return the lowest one
 function getLowestLocation(seeds: number[], maps: number[][][], seedRanges?: number[]): number {
   let lowestLocation = Number.MAX_SAFE_INTEGER;
   let s = 0;
@@ -32,32 +50,25 @@ function getLowestLocation(seeds: number[], maps: number[][][], seedRanges?: num
   return lowestLocation;
 }
 
+// solve part 1
 function solve1(input: string): number {
-  const split = input.split('\r\n\r\n');
-  const seeds = split[0].split(': ')[1].split(' ').map((e) => parseInt(e));
-  const mapsString = split.slice(1).map((e) => (e.split('\r\n').slice(1)));
-  const maps = mapsString.map((e) => e.map((i) => i.split(' ').map((j) => parseInt(j))));
-
+  const { seeds, maps } = parseInput(input);
   return getLowestLocation(seeds, maps);
 }
 
+// solve part 2
 function solve2(input: string): number {
-  const split = input.split('\r\n\r\n');
+  const { seeds: seedPairs , maps } = parseInput(input);
   const seeds = [];
   const seedRanges = [];
-  const seedPairs = split[0].split(': ')[1].split(' ').map((e) => parseInt(e));
   for (let index = 0; index < seedPairs.length / 2; index++) {
     seeds.push(seedPairs[index*2]);
     seedRanges.push(seedPairs[index*2+1]);
   }
-  const mapsString = split.slice(1).map((e) => (e.split('\r\n').slice(1)));
-  const maps = mapsString.map((e) => e.map((i) => i.split(' ').map((j) => parseInt(j))));
-
   return getLowestLocation(seeds, maps, seedRanges);
 }
 
 // Part 2 optimised by searching backwards (starting from a low number, search potential locations and go backwards finding the original seed, see if within seed ranges)
-
 function findOriginalSeed(seeds: number[], backwardsMap: number[][][], seedRanges: number[]): number {
   for (let i = 0; i < Number.MAX_SAFE_INTEGER; i++) {
     const orignalSeed = getLocation(i, backwardsMap);
@@ -70,17 +81,15 @@ function findOriginalSeed(seeds: number[], backwardsMap: number[][][], seedRange
   return -1;
 }
 
-function solveBackwards(input: string): number {
-  const split = input.split('\r\n\r\n');
+// solve part 2 by searching backwards
+function solve2Optimised(input: string): number {
+  const { seeds: seedPairs , maps } = parseInput(input);
   const seeds = [];
   const seedRanges = [];
-  const seedPairs = split[0].split(': ')[1].split(' ').map((e) => parseInt(e));
   for (let index = 0; index < seedPairs.length / 2; index++) {
     seeds.push(seedPairs[index*2]);
     seedRanges.push(seedPairs[index*2+1]);
   }
-  const mapsString = split.slice(1).map((e) => (e.split('\r\n').slice(1)));
-  const maps = mapsString.map((e) => e.map((i) => i.split(' ').map((j) => parseInt(j))));
   const backwardsMap = maps.map((e) => e.map((i) => [i[1], i[0], i[2]])).reverse();
   return findOriginalSeed(seeds, backwardsMap, seedRanges);
 }
@@ -90,11 +99,11 @@ const tfile = fs.readFileSync('./day5.test.txt', 'utf-8');
 
 console.log(`Test 1 solution: ${solve1(tfile)}`);
 console.log(`Test 2 solution: ${solve2(tfile)}`);
-console.log(`Test OPTIMISED solution: ${solveBackwards(tfile)}`);
+console.log(`Test OPTIMISED solution: ${solve2Optimised(tfile)}`);
 
 // main
 const file = fs.readFileSync('./day5.txt', 'utf-8');
 
 console.log(`Part 1 solution: ${solve1(file)}`);
 // console.log(`Part 2 solution : ${solve2(file)}`);
-console.log(`Part 2 OPTIMISED solution : ${solveBackwards(file)}`);
+console.log(`Part 2 OPTIMISED solution : ${solve2Optimised(file)}`);
